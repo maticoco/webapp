4179b804f8ef0a0e0f5768adf252d570bcf699a4
maticoco <162891008+maticoco@users.noreply.github.com>
2025-01-02 07:51:43 -0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="webApp",
-    api_url="https://back.cerrajerialjr.com.ar",
+    api_url="https://back.cerrajerialjr.com.ar:8000",
     db_url="sqlite:///reflex.db",
     
 )
