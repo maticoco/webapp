04dc0f2491c102de878128c4482552d4e8754809
maticoco <mati.cocoliche@gmail.com>
2025-09-17 12:06:18 -0300

    front ok todas las funciones, caddyfile y rxconfig deepseek

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,13 @@ import reflex as rx
 
 config = rx.Config(
     app_name="webApp",
-    api_url="https://webapp-tx8w.onrender.com",
+    api_url="https://back.cerrajerialjr.com.ar",
     db_url="sqlite:///reflex.db",
+    backend_host = "0.0.0.0",
+    cors_allowed_origins = [
+        "https://cerrajerialjr.com.ar",
+        "https://www.cerrajerialjr.com.ar",
+        "http://localhost:3000",  # Para desarrollo
+    ]
     
 )
